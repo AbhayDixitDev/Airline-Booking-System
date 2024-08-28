49a54341b72a4a94ffdbfaec7043733ec0090029
Abhay Dixit <dixitshivam534@gmail.com>
2024-08-28 16:29:37 +0530

    "Updated CSS, HTML, and JS files for dashboardCustomer and login functionality, added new user data to data.json, and modified login.js to handle admin login"

--- dashboardCustomer.js
+++ dashboardCustomer.js
@@ -12,3 +12,14 @@ function logincredentialscheck(){
       localStorage.setItem('loginChecked', true);
     }
   }
+
+  (function() {
+    const loginInfoStorage = localStorage.getItem('logininfo');
+    const loginInfo = JSON.parse(loginInfoStorage);
+    const name = loginInfo.name;
+    let customName = document.querySelector('#Welcome');
+    customName.innerHTML = `<h2>Hello <span style="color:white; font-size:70px"> ${name} </span>, </h2><h3>Welcome to Flight Booking System</h3> `;
+  })();
+
+ 
+
--- login.js
+++ login.js
@@ -15,9 +15,10 @@ const dashboardCustomer = async () => {
     console.log(finddata);
     // return false;
     if (finddata) {
-  
-      window.localStorage.setItem("logininfo", JSON.stringify(finddata));
       document.location.href = "dashboardCustomer.html";
+      localStorage.removeItem('logininfo');      
+      window.localStorage.setItem("logininfo", JSON.stringify(finddata));
+      
 
       
     } else {
@@ -28,6 +29,46 @@ const dashboardCustomer = async () => {
       if (choice) {
       } else {
         document.location.href = "customerreg.html";
+      }
+    }
+  
+    console.log(finddata);
+    return false;
+  };
+
+
+
+  const dashboardAdmin = async () => {
+    let data = await fetch("http://localhost:4000/Admin");
+    
+    let response = await data.json();
+    
+    console.log(response);
+    
+    let username = document.querySelector("#username").value;
+    let password = document.querySelector("#passwordAdmin").value;
+  
+    let finddata = response.find(
+      (e) => e.email === username && e.password === password
+    );
+  
+    console.log(finddata);
+    // return false;
+    if (finddata) {
+      document.location.href = "dashboardAdmin.html";
+
+      localStorage.removeItem('logininfo');      
+      window.localStorage.setItem("logininfo", JSON.stringify(finddata));
+
+      
+    } else {
+      var choice = confirm(
+        "Your credentials are incorrect. Click 'OK' to retry or 'Cancel' to register yourself."
+      );
+    
+      if (choice) {
+      } else {
+        alert('try again with correct credentials or contact developer');
       }
     }
   
